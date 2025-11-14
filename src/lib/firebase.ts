56b18e2344b34dc83b0be66ea77d1ca80dc90d86
Simon Brebeck <simonbrebeck@yahoo.de>
2025-11-14 22:12:08 +0200

    feat: add firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,8 @@
+import {firebaseConfig} from '@/lib/firebaseConfig';
+import { initializeApp } from 'firebase/app';
+import {
+    getFirestore,
+} from 'firebase/firestore';
+const app = initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(app);
