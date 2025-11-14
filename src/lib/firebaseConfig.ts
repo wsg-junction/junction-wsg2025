49076cedeec63b95b4923077b5cd0178cdccc591
Simon Brebeck <simonbrebeck@yahoo.de>
2025-11-14 22:15:14 +0200

    feat: add firebase config

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAfKvZy0XgyZgdCXnFbQO-3pX_gpWdbGu4",
+    authDomain: "junction2025-21668.firebaseapp.com",
+    projectId: "junction2025-21668",
+    storageBucket: "junction2025-21668.firebasestorage.app",
+    messagingSenderId: "468178422020",
+    appId: "1:468178422020:web:3fdb5229533dac2c232851",
+    measurementId: "G-0ZDCR55ME4"
+};
