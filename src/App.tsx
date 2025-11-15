2410a6f1e9ccbf4079a2f873e36333155d6f8a41
Simon Brebeck <simonbrebeck@yahoo.de>
2025-11-15 12:11:37 +0200

    feat: dashboard, language changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,38 @@
-import { AppSidebar } from "@/components/core/app-sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { Outlet } from "react-router";
-import { ThemeModeToggle } from "@/components/core/theme-mode-toggle.tsx";
+import { Outlet } from 'react-router';
 
 export default function App() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex me-4 justify-between h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-          <div>
-            <ThemeModeToggle />
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <Outlet />
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+    return <Outlet />;
+    /*
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex me-4 justify-between h-16 shrink-0 items-center gap-2">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator
+                            orientation="vertical"
+                            className="mr-2 data-[orientation=vertical]:h-4"
+                        />
+                        <Breadcrumb>
+                            <BreadcrumbList>
+                                <BreadcrumbItem className="hidden md:block">
+                                    <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator className="hidden md:block" />
+                                <BreadcrumbItem>
+                                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                    <div>
+                        <ThemeModeToggle />
+                    </div>
+                </header>
+                <div className="flex flex-1 flex-col gap-4 p-4 pt-0"></div>
+            </SidebarInset>
+        </SidebarProvider>
+    );
+     */
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,23 +1,35 @@
-import { createBrowserRouter } from "react-router";
-import App from "@/App.tsx";
-import DashboardPage from "@/pages/dashboard";
-import { ThemeProvider } from "@/components/core/theme-provider.tsx";
-import { NotFoundErrorPage } from "@/pages/error-pages/not-found.page.tsx";
+import { createBrowserRouter } from 'react-router';
+import App from '@/App.tsx';
+import DashboardPage from '@/pages/dashboard';
+import { ThemeProvider } from '@/components/core/theme-provider.tsx';
+import { NotFoundErrorPage } from '@/pages/error-pages/not-found.page.tsx';
+import CustomerShoppingPage from '@/pages/customers/customer-shopping';
 
 export const ROUTES = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ThemeProvider defaultTheme={"dark"} storageKey={"app-theme"}>
-        <App></App>
-      </ThemeProvider>
-    ),
-    errorElement: <NotFoundErrorPage></NotFoundErrorPage>,
-    children: [
-      {
-        index: true,
-        Component: DashboardPage,
-      },
-    ],
-  },
+    {
+        path: '/',
+        element: (
+            <ThemeProvider
+                defaultTheme={'dark'}
+                storageKey={'app-theme'}>
+                <App></App>
+            </ThemeProvider>
+        ),
+        errorElement: <NotFoundErrorPage></NotFoundErrorPage>,
+        children: [
+            {
+                index: true,
+                Component: DashboardPage,
+            },
+            {
+                path: 'customer',
+                children: [
+                    {
+                        index: true,
+                        Component: CustomerShoppingPage,
+                    },
+                ],
+            },
+        ],
+    },
 ]);
