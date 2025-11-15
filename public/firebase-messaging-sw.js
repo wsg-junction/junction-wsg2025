6bcc3cb91770503b8ac020cc26842d88ee807803
Jonas Wanke <contact@jonas-wanke.com>
2025-11-16 01:47:45 +0200

    Fix push notifications in foreground

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,37 +1,7 @@
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+self.addEventListener('push', function (event) {
+  const {
+    notification: { title, body },
+  } = JSON.parse(event.data.text());
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyAfKvZy0XgyZgdCXnFbQO-3pX_gpWdbGu4',
-  authDomain: 'junction2025-21668.firebaseapp.com',
-  projectId: 'junction2025-21668',
-  storageBucket: 'junction2025-21668.firebasestorage.app',
-  messagingSenderId: '468178422020',
-  appId: '1:468178422020:web:3fdb5229533dac2c232851',
-  measurementId: 'G-0ZDCR55ME4',
-});
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
-
-messaging.onMessage((payload) => {
-  console.log('Message received. ', payload);
-  // ...
-  self.registration.showNotification(payload.title, {
-    body: payload.body,
-    icon: '/firebase-logo.png',
-  });
-});
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png',
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  event.waitUntil(self.registration.showNotification(title, { body }));
 });
