077a9768fe32e7820c09101236835fe2a23ffeaa
Simon Brebeck <simonbrebeck@yahoo.de>
2025-11-15 16:18:45 +0200

    feat: add more grid cols

--- src/pages/customers/customer-shopping/index.tsx
+++ src/pages/customers/customer-shopping/index.tsx
@@ -79,7 +79,7 @@ export default function CustomerShoppingPage() {
                 </Breadcrumb>
                 <div className="hero"></div>
             </div>
-            <div className="px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div className="px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6">
                 {allProducts.map((product, index) => {
                     const { name, description, price } = product;
                     const formattedPrice = price.value ? price.value.toFixed(2) : '0.00';
